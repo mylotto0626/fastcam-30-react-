22a580b992ce84cb8e60ac856e531740f640cfc7
summer <gupy1998@gmail.com>
2025-03-13 15:09:24 +0900

    Fix: Re-add virtual-keyboard as a regular folder

---
+++ virtual-keyboard/README.md
@@ -0,0 +1,2 @@
+# fastcampus-30-react
+패스트캠퍼스 30개 프로젝트로 배우는 프론트엔드 with React 공부 레포지토리 
---
+++ virtual-keyboard/src/js/index.js
@@ -0,0 +1,6 @@
+import "../css/style.css";
+
+import { Keyboard } from "./keyboard";
+new Keyboard();
+
+// 왜 안올라가냐고..
